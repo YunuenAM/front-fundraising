3a630df0f3fe755112afb24c408a2027b5efe318
Yunuen AM <yunameza@outlook.com>
2023-10-19 21:19:31 -0600

    ✨Feat:  routing with react router dom

--- 01-react-fundraising/src/pages/App/index.jsx
+++ 01-react-fundraising/src/pages/App/index.jsx
@@ -1,19 +1,28 @@
-import '@/App.css'
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import Dashboard from '../Dashboard'
 import Login from '../Login.jsx'
 import Notfound from '../Notfound'
 import Signup from '../Signup'
+import '@/App.css'
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/dashboard', element: <Dashboard /> },
+    { path: '/login', element: <Login /> },
+    { path: '/*', element: <Notfound /> },
+    { path: '/sign-up', element: <Signup /> }
+
+  ])
+  return routes
+}
 
 const App = () => {
   return (
-    <div className='bg-pink'>
-      <Home />
-      <Dashboard />
-      <Login />
-      <Notfound />
-      <Signup />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 export default App
