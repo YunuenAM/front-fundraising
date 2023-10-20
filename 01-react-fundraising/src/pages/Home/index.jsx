5d9073372b82fdfcedb16440763a7d082ca4e2b2
Yunuen AM <yunameza@outlook.com>
2023-10-20 15:50:19 -0600

    ✨Feat:  colors  updated

--- 01-react-fundraising/src/pages/Home/index.jsx
+++ 01-react-fundraising/src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <Layout>
       <img className=' mx-auto d-block  rounded-circle p-1 mt-4   ' src={axolotl} alt='axolotl image' style={{ maxWidth: '50rem' }} />
+
     </Layout>
   )
 }
