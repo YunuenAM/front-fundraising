cfe10062bdc143c6ba9974e04dac1fdec3fe655e
Yunuen AM <yunameza@outlook.com>
2023-10-19 20:59:39 -0600

    ✨Feat:  App updated

--- 01-react-fundraising/src/pages/App/index.jsx
+++ 01-react-fundraising/src/pages/App/index.jsx
@@ -1,8 +1,19 @@
 import '@/App.css'
+import Home from '../Home'
+import Dashboard from '../Dashboard'
+import Login from '../Login.jsx'
+import Notfound from '../Notfound'
+import Signup from '../Signup'
 
 const App = () => {
   return (
-    <div className='bg-pink'>Save the Axolote</div>
+    <div className='bg-pink'>
+      <Home />
+      <Dashboard />
+      <Login />
+      <Notfound />
+      <Signup />
+    </div>
   )
 }
 export default App
