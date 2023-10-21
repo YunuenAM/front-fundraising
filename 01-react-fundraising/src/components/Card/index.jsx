f343b9bb0a15902762f2e62d7db7c85f8b1a63ee
Yunuen AM <yunameza@outlook.com>
2023-10-20 19:44:06 -0600

    ✨Feat:   cards height updated

--- 01-react-fundraising/src/components/Card/index.jsx
+++ 01-react-fundraising/src/components/Card/index.jsx
@@ -2,59 +2,63 @@ import axolotl from '@/img/axolotl.jpg'
 import axolotl2 from '@/img/axolotl2.jpg'
 import axolotl3 from '@/img/axolotl3.jpg'
 import axolotl4 from '@/img/axolotl4.jpg'
+import axolotl7 from '@/img/axolotl7.jpg'
+import babyaxolotl2 from '@/img/babyaxolotl2.jpg'
+import babyaxolotl from '@/img/babyaxolotl.jpg'
+import axolotl9 from '@/img/axolotl9.jpg'
 
 const Card = () => {
   const cardData = [
     {
       id: 1,
       imgSrc: axolotl,
-      title: 'Card 1',
-      content: 'Content for Card 1'
+      title: 'Axo',
+      content: 'Help Axo keep swimming happily in his habitat! Your generous donation will ensure a clean and safe environment, securing a bright future for this adorable axolotl. Join us in preserving the beauty of nature and supporting axo on his journey! 🌊🐟💙 #SaveAXO'
     },
     {
       id: 2,
       imgSrc: axolotl2,
-      title: 'Card 2',
-      content: 'Content for Card 2'
+      title: 'Lottie',
+      content: 'Help Lottie, the adorable axolotl, thrive in her natural habitat! Axolotls are an endangered species, and with your contribution, we can ensure that Lottie and her friends have a safe home. Your generosity will make a difference in the conservation of these fascinating aquatic animals. Donate now and be part of the mission to save Lottie and her species! Every small contribution matters.✨🪸💛#SaveLOTTIE'
     },
     {
 
       id: 3,
       imgSrc: axolotl3,
-      title: 'Card 2',
+      title: 'Salamandria',
       content: 'Content for Card 2'
     },
     {
       id: 4,
       imgSrc: axolotl4,
-      title: 'Card 1',
+      title: 'Gilly',
       content: 'Content for Card 1'
     },
     {
       id: 5,
-      imgSrc: './img/axolotl2.jpg',
-      title: 'Card 2',
+      imgSrc: axolotl7,
+      title: 'Amphie',
       content: 'Content for Card 2'
     },
     {
 
       id: 6,
-      imgSrc: './img/axolotl3.jpg',
-      title: 'Card 2',
+      imgSrc: babyaxolotl2,
+      title: 'Kiko',
       content: 'Content for Card 2'
     },
     {
 
       id: 7,
-      imgSrc: './img/axolotl3.jpg',
-      title: 'Card 2',
+      imgSrc: babyaxolotl,
+      title: 'Quaxo',
       content: 'Content for Card 2'
     },
     {
 
       id: 8,
-      imgSrc: './img/axolotl3.jpg',
-      title: 'Card 2',
+      imgSrc: axolotl9,
+      title: 'Marlowe',
       content: 'Content for Card 2'
     }
 
@@ -64,7 +68,7 @@ const Card = () => {
     <div className='row'>
       {cardData.map((card) => (
         <div className='col-md-3' key={card.id}>
-          <div className='card' style={{ width: '18rem', margin: '50px' }}>
+          <div className='card' style={{ width: '18rem', margin: '50px 0', height: '90%' }}>
             <img src={card.imgSrc} className='card-img-top' alt={card.title} />
             <div className='card-body'>
               <h5 className='card-title'>{card.title}</h5>
