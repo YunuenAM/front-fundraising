13eaafb27fc9339fe9372ff6e1d342706a0dde73
Yunuen AM <yunameza@outlook.com>
2023-10-25 13:54:27 -0600

    ✨Feat: useAuth hook added

---
+++ 01-react-fundraising/src/hooks/useAuth.jsx
@@ -0,0 +1,11 @@
+import { useContext } from 'react'
+import { AuthContext } from '@/context/AuthContext'
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext)
+
+  if (context === undefined) {
+    throw new Error('useAuthContext should be used inside the AuthProvider')
+  }
+  return context
+}
